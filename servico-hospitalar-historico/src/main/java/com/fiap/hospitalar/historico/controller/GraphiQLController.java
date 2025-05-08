1f2438509e614fde826dd1019dff86c6106bfcb7
Alexsandro <amouraorr@yahoo.com.br>
2025-05-08 07:52:44 -0300

    Ajustes implementações graphql.

--- servico-hospitalar-historico/src/main/java/com/fiap/hospitalar/historico/controller/GraphiQLController.java
+++ servico-hospitalar-historico/src/main/java/com/fiap/hospitalar/historico/controller/GraphiQLController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GraphiQLController {
+
     @GetMapping("/graphiql")
     @ResponseBody
     public String graphiql() {
@@ -14,20 +15,24 @@ public class GraphiQLController {
                 "<html>" +
                 "<head>" +
                 "<meta charset=utf-8/>" +
-                "<title>GraphQL Playground</title>" +
-                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no\"/>" +
-                "<link rel=\"stylesheet\" href=\"//cdn.jsdelivr.net/npm/graphql-playground-react@1.8.10/build/static/css/index.css\" />" +
-                "<link rel=\"shortcut icon\" href=\"//cdn.jsdelivr.net/npm/graphql-playground-react@1.8.10/build/favicon.png\" />" +
-                "<script src=\"//cdn.jsdelivr.net/npm/graphql-playground-react@1.8.10/build/static/js/middleware.js\"></script>" +
+                "<title>GraphiQL</title>" +
+                "<link rel=\"stylesheet\" href=\"https://unpkg.com/graphiql/graphiql.css\" />" +
+                "<script src=\"https://unpkg.com/react/umd/react.production.min.js\"></script>" +
+                "<script src=\"https://unpkg.com/react-dom/umd/react-dom.production.min.js\"></script>" +
+                "<script src=\"https://unpkg.com/graphiql/graphiql.min.js\"></script>" +
                 "</head>" +
                 "<body>" +
-                "<div id=\"root\"></div>" +
-                "<script>window.addEventListener('DOMContentLoaded', function (event) {" +
-                "GraphQLPlayground.init(document.getElementById('root'), {" +
-                "endpoint: '/graphql'" +
-                "})" +
-                "})</script>" +
+                "<div id=\"graphiql\">Loading...</div>" +
+                "<script>" +
+                "const graphQLFetcher = (graphQLParams) => fetch('/graphql', {" +
+                "method: 'POST'," +
+                "headers: {'Content-Type': 'application/json'}," +
+                "body: JSON.stringify(graphQLParams)," +
+                "}).then(response => response.json()).catch(error => console.error(error));" +
+                "ReactDOM.render(React.createElement(GraphiQL, { fetcher: graphQLFetcher }), document.getElementById('graphiql'));" +
+                "</script>" +
                 "</body>" +
                 "</html>";
     }
+
 }
