605d276a35da6ed9dd4888aaf9a3a8c078513f93
Alexsandro <amouraorr@yahoo.com.br>
2025-05-11 20:52:50 -0300

    Ajustes implementações.

--- servico-hospitalar-historico/src/main/java/com/fiap/hospitalar/historico/controller/GraphQLController.java
+++ servico-hospitalar-historico/src/main/java/com/fiap/hospitalar/historico/controller/GraphQLController.java
@@ -3,30 +3,42 @@ package com.fiap.hospitalar.historico.controller;
 import com.fiap.hospitalar.historico.dto.HistoryDTO;
 import com.fiap.hospitalar.historico.mapper.HistoryMapper;
 import com.fiap.hospitalar.historico.model.History;
+import com.fiap.hospitalar.historico.service.HistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class GraphQLController {
 
+    @Autowired
+    private HistoryService historyService;
+
     @QueryMapping
     public List<HistoryDTO> getPatientHistory(@Argument String paciente) {
+        List<History> histories = historyService.getConsultationsByPatientId(paciente);
+        return histories.stream()
+                .map(HistoryMapper.INSTANCE::historyToHistoryDTO)
+                .collect(Collectors.toList());
+    }
 
+    @MutationMapping
+    public HistoryDTO addHistory(@Argument String paciente,
+                                 @Argument String medico,
+                                 @Argument String enfermeiro,
+                                 @Argument String dataHora) {
         History history = new History();
-        history.setId(1L);
         history.setPaciente(paciente);
-        history.setMedico("Dr. Silva");
-        history.setEnfermeiro("Enf. Souza");
-        history.setDataHora(LocalDateTime.parse("2025-05-09T18:30:00"));
-
-        HistoryDTO dto = HistoryMapper.INSTANCE.historyToHistoryDTO(history);
-
-        return Collections.singletonList(dto);
-
+        history.setMedico(medico);
+        history.setEnfermeiro(enfermeiro);
+        history.setDataHora(LocalDateTime.parse(dataHora));
+        History savedHistory = historyService.saveHistory(history);
+        return HistoryMapper.INSTANCE.historyToHistoryDTO(savedHistory);
     }
 }
