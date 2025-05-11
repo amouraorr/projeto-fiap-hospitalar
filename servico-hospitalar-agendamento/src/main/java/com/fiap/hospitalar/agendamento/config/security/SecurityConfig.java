e18b8a3f72d65ee00cd93673734231cbd4e55d14
Alexsandro <amouraorr@yahoo.com.br>
2025-05-11 20:50:26 -0300

    Ajustes implementações.

--- servico-hospitalar-agendamento/src/main/java/com/fiap/hospitalar/agendamento/config/security/SecurityConfig.java
+++ servico-hospitalar-agendamento/src/main/java/com/fiap/hospitalar/agendamento/config/security/SecurityConfig.java
@@ -2,11 +2,8 @@ package com.fiap.hospitalar.agendamento.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,41 +19,4 @@ public class SecurityConfig  {
                 );
         return http.build();
     }
-
-
-/*@Bean
-    public PasswordEncoder passwordEncoder() {
-
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll() // Permitir acesso a todas as requisições
-                )
-                .csrf(csrf -> csrf.disable()) // Desabilita CSRF para facilitar os testes
-                .httpBasic(Customizer.withDefaults());
-
-        return http.build();
-    }
-
-
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/consultas/**").hasRole("MEDICO")
-                        .requestMatchers("/historico/**").hasAnyRole("MEDICO", "ENFERMEIRO")
-                        .requestMatchers("/minhas-consultas/**").hasRole("PACIENTE")
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
-
-        return http.build();
-    }*/
-
-
 }
